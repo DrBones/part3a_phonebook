362d0b4f3ef45797e8f6afb65aa0147b83434835
DrBones <DrBones@users.noreply.github.com>
2024-09-21 12:40:08 +0200

    commit before changing this to the phonebook exercise

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 console.log("Hello, World");
+import express from "express";
+const app = express();
 
-import http from "http";
+app.use(express.json());
 
 let notes = [
   {
@@ -20,11 +22,53 @@ let notes = [
   },
 ];
 
-const app = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(JSON.stringify(notes));
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, World!</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.send(notes);
+});
+
+app.get("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  const note = notes.find((note) => note.id === id);
+
+  if (note) {
+    res.send(note);
+  } else {
+    res.statusMessage = "No note with that id found";
+    res.status(404).end();
+  }
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  notes = notes.filter((note) => note.id !== id);
+  res.status(204).end();
+});
+
+const generateID = () => {
+  const maxID =
+    notes.length > 0 ? Math.max(...notes.map((note) => Number(note.id))) : 0;
+  return String(maxID + 1);
+};
+
+app.post("/api/notes", (req, res) => {
+  const body = req.body;
+
+  if (!body.content) {
+    return res.status(400).json({ error: "Content is Missing" });
+  }
+  const note = {
+    content: body.content,
+    important: Boolean(body.important) || false,
+    id: generateID(),
+  };
+  notes = notes.concat(note);
+  res.json(note);
+  console.log(note);
 });
 
 const PORT = 3001;
-app.listen(PORT);
-console.log(`Server is running on port ${PORT}`);
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
