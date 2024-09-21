a8c535682aeafceb39d543f3099ccbefb0cbf897
DrBones <DrBones@users.noreply.github.com>
2024-09-21 23:35:27 +0200

    Update index.js
    Port now chosen from process.env or hardvodes PORT

--- index.js
+++ index.js
@@ -70,5 +70,5 @@ app.post("/api/notes", (req, res) => {
   console.log(note);
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
