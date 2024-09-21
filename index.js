908e9cd2e28da53eb63393b52caa1e09f5b1f787
DrBones <DrBones@users.noreply.github.com>
2024-09-21 23:59:08 +0200

    cleaning up merge

--- index.js
+++ index.js
@@ -107,5 +107,3 @@ app.post("/api/persons", (req, res) => {
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
