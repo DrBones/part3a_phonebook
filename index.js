104145e1f7de45743034903077f82e2740f1c664
DrBones <DrBones@users.noreply.github.com>
2024-09-22 13:32:03 +0200

    setup for frontend deployment with backend

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const app = express();
 //   next();
 // };
 app.use(express.json());
+app.use(express.static("dist"));
 // app.use(requestLogger);
 morgan.token("type", function (req, res) {
   return JSON.stringify(req.body);
